3f7bc3b43745fedd165f7fa1b1186a27758aeeb0
KrenZfer <krenzferalfha@gmail.com>
2020-10-08 16:36:50 +0700

    support js attribute

--- resources/views/partials/js.blade.php
+++ resources/views/partials/js.blade.php
@@ -1,3 +1,11 @@
 @foreach($js as $j)
+@if(strpos($j, "&") !== false)
+<?php 
+    $js_attribute = explode('#', $j);
+
+?>
+<script src="{{ admin_asset ("$js_attribute[0]") }}" {{$js_attribute[1]}}></script>
+@else
 <script src="{{ admin_asset ("$j") }}"></script>
+@endif
 @endforeach
