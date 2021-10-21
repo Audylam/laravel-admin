29aebfc674f5698ce35c3263f6ca2f2ac33044ad
Audylam <flash25.ag@gmail.com>
2021-10-21 10:05:38 +0700

    Update CanDoubleClick.php
    add new line at the end of script

--- src/Grid/Concerns/CanDoubleClick.php
+++ src/Grid/Concerns/CanDoubleClick.php
@@ -18,7 +18,10 @@ $('body').on('dblclick', 'table#{$this->tableID}>tbody>tr', function(e) {
     var url = "{$this->resource()}/"+$(this).data('key')+"/edit";
     $.admin.redirect(url);
 });
-SCRIPT);
+
+SCRIPT
+
+    );
 
         return $this;
     }
